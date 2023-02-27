2b3713fce46a3e185da6b7cf87e0abcf2fbc0e09
Robin Schyboll <robin.schyboll@gmail.com>
2023-02-27 19:27:46 +0000

    feat: Cleaned up the project

--- old/README.md
+++
@@ -1,1 +0,0 @@
-# rschyboll.github.io
--- old/src/pages/footer/model.ts
+++
@@ -1,9 +0,0 @@
-import { SiGithub, SiLinkedin } from 'react-icons/si';
-
-export const Links = {
-  github: { icon: SiGithub, link: 'https://github.com/rschyboll' },
-  linkedIn: {
-    icon: SiLinkedin,
-    link: 'https://linkedin.com/in/robin-schyboll',
-  },
-};
