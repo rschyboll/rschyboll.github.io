4bc11cd21d76ccaf5e1ee03dc0e870d789168bd6
Robin Schyboll <robin.schyboll@gmail.com>
2023-04-07 11:18:47 +0000

    feat: Removed server code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+	import { locale } from '$lib/translations';
+
+	$: console.log($locale);
+</script>
+
 <slot />
+
+<button on:click={() => locale.set('en')}>EN</button>
+<button on:click={() => locale.set('pl')}>PL</button>
+<button on:click={() => locale.set('de')}>DE</button>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,5 +6,4 @@ export const load: Load = async ({ url }) => {
 	const lang = `${pathname.match(/\w+?(?=\/|$)/) || ''}`;
 	const route = pathname.replace(new RegExp(`^/${lang}`), '');
 	await loadTranslations(lang, route);
-	return { stuff: { route, lang } };
 };
